832652f5dd38f6c0e219374fae4d2ff9547059ca
Michael Mukiibi <michael.mukiibi.k@gmail.com>
2025-06-27 18:56:05 +0300

    fix: add inheritance to Box contract

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -6,17 +6,17 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 // Make Box inherit from the Ownable contract
-contract Box {
+contract Box is Ownable(msg.sender) {
     uint256 private _value;
 
     // Emitted when the stored value changes
     event ValueChanged(uint256 value);
 
-    constructor() Ownable(msg.sender) {}
+    constructor() {}
 
     // Stores new value in the contract
     // The onlyOwner modifier restricts who can call the store function
-    function store(uint256 value) public {
+    function store(uint256 value) public onlyOwner {
         _value = value;
         emit ValueChanged(value);
     }
