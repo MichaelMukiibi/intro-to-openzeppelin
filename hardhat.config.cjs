50fe6c91897ab24b2947aef62b3d25714647da2d
Michael Mukiibi <michael.mukiibi.k@gmail.com>
2025-06-28 05:11:11 +0300

    feat: updated testnet endpoints

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,13 +1,13 @@
 require("@nomicfoundation/hardhat-ethers");
 
-const { alchemyApiKey, mnemonic } = require('./secrets.json')
+const { testnetUrl, testnetApiKey, mnemonic } = require('./secrets.json')
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
   networks: {
     sepolia: {
-      url: `https://eth-sepolia.g.alchemy/v2/${alchemyApiKey}`,
+      url: `${testnetUrl}/${testnetApiKey}`,
       accounts: { mnemonic: mnemonic },
     },
   },
