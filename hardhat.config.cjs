5c7418e7a38b0264972b771e655b498c5ca10893
Michael Mukiibi <michael.mukiibi.k@gmail.com>
2025-06-28 04:49:31 +0300

    feat: added mnemonic and testnet config

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-ethers");
 
+const { alchemyApiKey, mnemonic } = require('./secrets.json')
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy/v2/${alchemyApiKey}`,
+      accounts: { mnemonic: mnemonic },
+    },
+  },
 };
