7551b893e0e8db98129df425fb43dc393082ac6a
Michael Mukiibi <michael.mukiibi.k@gmail.com>
2025-06-28 03:21:06 +0300

    feat: retrieved box value from deployed contract

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,14 @@ import pkg from "hardhat";
 const { ethers } = pkg;
 
 async function main() {
-    // Retrieve accounts from the local node
-    const accounts = (await ethers.getSigners()).map(signer => signer.address);
-    console.log(accounts);
+    // Set uo an ethers contract, representing our deployed Box instance
+    const address = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
+    const Box = await ethers.getContractFactory('Box');
+    const box = Box.attach(address);
+
+    // Retrieve the current value of the box
+    const value = await box.retrieve();
+    console.log('Box value is', value.toString());
 }
 
 main()
